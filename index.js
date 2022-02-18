41b7475dba09ef424802a4d6acc6eb79f33d3cec
sunilkarki <karkisunil1200@gmail.com>
2022-02-18 13:56:31 -0600

    Added mongoose and express

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hello");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://skarki:Nepal123@cluster0.pgouj.mongodb.net/shop?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("DBConnection Successfull"))
+  .catch((err) => console.log(err));
+
+app.listen(5000, () => {
+  console.log("Backend server is running....");
+});
