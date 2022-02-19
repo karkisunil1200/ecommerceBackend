6cb141fa27ee9a586bf58e8b076a0fecd6bd24b5
sunilkarki <karkisunil1200@gmail.com>
2022-02-18 18:35:07 -0600

    Added login and register endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const CryptoJs = require("crypto-js");
+const CryptoJS = require("crypto-js");
 
 // Register
 router.post("/register", async (req, res) => {
@@ -22,5 +22,25 @@ router.post("/register", async (req, res) => {
 });
 
 // Login
+
+router.post("/login", async (req, res) => {
+  console.log("I was here ");
+  try {
+    const user = await User.findOne({ username: req.body.username });
+
+    !user && res.status(401).json("Wrong Credentials");
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+    const password = hashedPassword.toString(CryptoJS.enc.Utf8);
+    password !== req.body.password && res.status(401).json("Wrong credentials");
+
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
