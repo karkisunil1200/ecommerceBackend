a54000dc6d791551c237b3b55043da57c506d20a
sunilkarki <karkisunil1200@gmail.com>
2022-02-18 18:18:40 -0600

    Added CryptoJs

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,16 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const CryptoJs = require("crypto-js");
 
 // Register
 router.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: CryptoJs.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
   });
   try {
     const savedUser = await newUser.save();
